b416b138269eb64781b03864ffe841d82b299dfb
Jackman3005 <Jackman3000@gmail.com>
2013-09-23 19:33:59 -0400

    Jack - Made new package with stub class.

---
+++ Checkers/src/com/checkers/ArtificialIntelligence/SmartMoveMaker.java
@@ -0,0 +1,16 @@
+package com.checkers.ArtificialIntelligence;
+
+import java.util.List;
+
+import com.checkers.model.CheckersPieceModel;
+import com.checkers.model.PlayerToken;
+import com.checkers.model.PossibleMove;
+
+public class SmartMoveMaker {
+
+	public PossibleMove getBestMoveToMakeForPlayer(
+			List<CheckersPieceModel> allCheckersPieces, PlayerToken playerToken) {
+		return null;
+	}
+
+}
